c8a99a23a978a4d1a9491c630b9f7248acae18f3
luke123adams <lxa207@gmail.com>
2023-10-09 16:33:40 +0100

    added section divider and about section

---
+++ portfolio/components/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section>
+        <h2 className='text-3xl font-medium capitalize mb-8'>About me</h2>
+        <p className='mb-3'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae rhoncus lorem. Vivamus accumsan cursus faucibus. Cras eleifend semper justo vel molestie. Pellentesque felis odio, dapibus sed pharetra in, pulvinar nec felis. Pellentesque ut pretium nisi. Fusce vestibulum nunc in eros vulputate, at luctus nisi rhoncus. In tincidunt purus aliquam nisl rhoncus, sed vestibulum libero venenatis. Cras eu mauris ligula.
+        </p>
+        <p>
+        Fusce et aliquam ligula. Quisque aliquet viverra dapibus. Duis mi urna, tempus sed mauris eu, laoreet lobortis mauris. Integer id mi urna. Proin a ligula viverra, rutrum ligula vel, eleifend justo. Fusce elementum dolor at est tristique luctus. Etiam dignissim lobortis eros, sit amet semper leo lacinia vel. Nam ac nisl vel elit auctor sodales. Fusce augue risus, sodales vitae cursus id, varius at mauris. Mauris vel libero lobortis, porttitor metus quis, pulvinar ligula. Nam eu ante ac orci pellentesque venenatis vel id nisi. Nulla dictum risus mauris, in volutpat erat gravida eu. Praesent id dolor ullamcorper, tempus purus nec, commodo velit. Sed vel nisl ex. Nunc mattis nibh ex, sed sodales mi porttitor at. Donec iaculis sit amet lectus non accumsan.
+        </p>
+    </section>
+  )
+}
--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { BsArrowRight, BsLinkedin } from "react-icons/bs"
 import { HiDownload } from "react-icons/hi"
 import { FaGithubSquare } from 'react-icons/fa'
+import SectionDivider from './section-divider'
 
 export default function Intro() {
   return (
@@ -37,7 +38,7 @@ export default function Intro() {
                  >👋</motion.span>
             </motion.div>
             </div>
-            <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             >
@@ -46,7 +47,7 @@ export default function Intro() {
         for my first developer role. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.   
-            </motion.p>
+            </motion.h1>
             <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
@@ -56,7 +57,7 @@ export default function Intro() {
                 <Link href="#contact"
                 className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-grey-950 focus:scale-110 active:scale-105 transition'>
                     Contact me {" "}
-                    <BsArrowRight className="opacity-70 group-hover:translate-x-2 transition" />
+                    <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
 
                 <a className="group bg-white text-black px-7 py-3
@@ -67,15 +68,21 @@ export default function Intro() {
                     Download CV{" "} <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
                     </a>
 
-                    <a className="bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full hover:scale-110 focus:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
+                    <a className="bg-white text-gray-700 hover:text-gray-950 px-7 py-3 flex items-center gap-2 rounded-full hover:scale-110 focus:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+                    href="https://www.linkedin.com/in/luke-adams-codes/"
+                    target="_blank"
+                    >
                         <BsLinkedin />
                     </a>
 
-                    <a className="bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full text-[1.35rem] hover:scale-110 focus:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10">
+                    <a className="bg-white text-gray-700 hover:text-gray-950 px-7 py-3 flex items-center gap-2 rounded-full text-[1.35rem] hover:scale-110 focus:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+                    href="https://www.github.com/luke123adams"
+                    target="_blank">
                         <FaGithubSquare />
                     </a>
             </motion.div>
             
             </section>
+            
   )
 }
