0ed597be514167e78b1f3fcda3fb369a7d215fc8
luke123adams <lxa207@gmail.com>
2023-10-12 11:17:56 +0100

    completed skills and experience sections

---
+++ portfolio/components/contact.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Contact() {
+  return (
+    <SectionHeading>Contact Me</SectionHeading>
+  )
+}
---
+++ portfolio/components/skills.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import React from 'react'
+import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
+import { useSectionInView } from '@/lib/hooks'
+import { motion } from 'framer-motion'
+
+const fadeInAnimationVariants = {
+    initial: {
+        opacity: 0,
+        y: 100,
+    },
+    animate: (index: number) =>  ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.05 * index
+        }
+    }),
+
+}
+
+export default function Skills() {
+
+    const { ref } = useSectionInView('Skills', 0.5)
+
+  return (
+    <section id="skills" ref={ref} className="mb-28 max-2-[53rem] text-center scroll-mt-28 sm:mb-40">
+        <SectionHeading>My Skills</SectionHeading>
+        <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+            {
+                skillsData.map((skill, i)=> (
+                    <motion.li
+                    variants={fadeInAnimationVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{
+                        once: true
+                    }}
+                    custom={i}
+                    className='bg-white border border-black/[0.1] rounded-xl px-5 py-3' key={i}>{skill}</motion.li>
+                ))
+            }
+        </ul>
+    </section>
+  )
+}
