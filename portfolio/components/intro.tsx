095eeb7f51ba43dd287e84f1b07dbc891b2ad4f4
luke123adams <112545296+luke123adams@users.noreply.github.com>
2026-06-12 11:41:47 +0100

    Update intro.tsx

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -54,7 +54,7 @@ export default function Intro() {
             >
             <span className="font-bold">Hello, I'm Luke.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> looking{" "}
-        for my first developer role. I enjoy
+        for my next developer role. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.   
             </motion.h1>
